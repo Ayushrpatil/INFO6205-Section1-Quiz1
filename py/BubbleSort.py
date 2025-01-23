1eb9a231e684688a375a1f3c08183765a821ea52
Ayushrpatil <patil.ay@northeastern.edu>
2025-01-23 13:24:52 -0500

    BubbleSort solution

--- py/BubbleSort.py
+++ py/BubbleSort.py
@@ -18,5 +18,14 @@ def bubble_sort(arr):
         """
         arr[i], arr[j] = arr[j], arr[i]
 
-    # TODO: Implement the bubble sort algorithm below.
-    pass
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                swap(j, j + 1)
+                swapped = True
+        if not swapped:
+            break
+
+    return arr
